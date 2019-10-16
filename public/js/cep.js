28ac43e098967aa505ffedded672bf6cf45604db
PauloMarcusso <phmarcusso1@hotmail.com>
2019-10-16 10:19:39 -0300

    add API CEP

---
+++ public/js/cep.js
@@ -0,0 +1,23 @@
+const cepInput = document.querySelector('#cep');
+const cidadeInput = document.querySelector('#cidade');
+const ufInput = document.querySelector('#uf');
+
+const buscaCep = (evt) => {
+
+    const cepDigitado = evt.target.value; 
+
+    if(cepDigitado.length != 8 ){
+        //saindo da função (return vazio)
+        return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
+    .then((response) => {return response.json()})
+    .then((dados) => {
+    cidadeInput.value = dados.localidade;
+    ufInput.value = dados.uf;
+    cepInput.value = dados.cep;
+    })
+}
+
+cepInput.oninput = buscaCep;
